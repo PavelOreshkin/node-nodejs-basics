6bb53e1a789b7071a7bf541b3dff160928548651
PavelOreshkin <poreshkin@samokat.ru>
2024-10-05 00:08:41 +0400

    feat: solve hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filename = `${__dirname}\\files\\fileToCalculateHashFor.txt`;
+  const hash = createHash("sha256");
+
+  const input = createReadStream(filename);
+
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(`>>> ${hash.digest("hex")} ${filename}`);
+    }
+  });
 };
 
 await calculateHash();
